1494a08d44d0ee6634630d1e34dc99703dec1213
liujd <liujd@shanghaitech.edu.cn>
2019-05-30 16:16:53 +0800

    add response functions

--- include/MaxPoolLayer.hpp
+++ include/MaxPoolLayer.hpp
@@ -5,54 +5,53 @@
 
 #include <vector>
 
+
+
 class MaxPoolLayer : public Layer {
 public:
-    MaxPoolLayer(std::vector<int> param, std::function<double(double)> rf);
-    Eigen::Tensor<double,3> calculate(Eigen::Tensor<double,3> input_data);
-    ~MaxPoolLayer();
+    MaxPoolLayer(std::vector<int> param, std::function<double(double)> rf)
+    : layer_param(param), layer_response_function(rf) {}
+
+    double window_max(Eigen::Tensor<double,3> input_data, int i, int begin_row, int end_row, int begin_col, int end_col)
+    {
+        double max=0.0;
+        for(int j=begin_row;j<end_row;j++){
+            for(int k=begin_col;k<end_col;k++){
+                if(input_data(i,j,k)>max){
+                    max=input_data(i,j,k);
+                }
+            }
+        }
+        return max;
+    }
+
+    Eigen::Tensor<double,3> calculate(Eigen::Tensor<double,3> input_data)
+    {
+        int result_row=input_data.dimension(1)/layer_param[0];
+        int result_col=input_data.dimension(2)/layer_param[1];
+        Eigen::Tensor<double,3> result(input_data.dimension(0),result_row,result_col) ;
+
+        for (int i = 0; i < result.dimension(0); i++)
+        {
+            for (int j = 0; j < result.dimension(1); j++)
+            {
+                for (int k = 0; k < result.dimension(2); k++)//may wrong!!
+                {
+                    result(i, j, k) = window_max(input_data,i,(j*layer_param[0]),((j+1)*layer_param[0]),(k*layer_param[1]),((k+1)*layer_param[1]));
+                    result(i, j, k) = layer_response_function(result(i, j, k));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    ~MaxPoolLayer() {}
 
 private:
     std::vector<int> layer_param;
     std::function<double(double)> layer_response_function;
 };
 
-MaxPoolLayer::MaxPoolLayer(std::vector<int> param,
-                                                     std::function<double(double)> rf)
-    : layer_param(param), layer_response_function(rf) {}
-
-MaxPoolLayer::~MaxPoolLayer() {}
-
-double window_max(Eigen::Tensor<double,3> input_data, int i, int begin_row, int end_row, int begin_col, int end_col){
-    double max=0.0;
-    for(int j=begin_row;j<end_row;j++){
-        for(int k=begin_col;k<end_col;k++){
-            if(input_data(i,j,k)>max){
-                max=input_data(i,j,k);
-            }
-        }
-    }
-    return max;
-}
-
-Eigen::Tensor<double,3> MaxPoolLayer::calculate(Eigen::Tensor<double,3> input_data)
-{
-    int result_row=input_data.dimension(1)/layer_param[0];
-    int result_col=input_data.dimension(2)/layer_param[1];
-    Eigen::Tensor<double,3> result(input_data.dimension(0),result_row,result_col) ;
-
-    for (int i = 0; i < result.dimension(0); i++)
-    {
-        for (int j = 0; j < result.dimension(1); j++)
-        {
-            for (int k = 0; k < result.dimension(2); k++)//may wrong!!
-            {
-                result(i, j, k) = window_max(input_data,i,(j*layer_param[0]),((j+1)*layer_param[0]),(k*layer_param[1]),((k+1)*layer_param[1]));
-                result(i, j, k) = layer_response_function(result(i, j, k));
-            }
-        }
-    }
-
-    return result;
-}
 
 #endif
