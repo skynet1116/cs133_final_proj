e504c8af50fc77077c163c7de24da82650ba12d4
yujy <yujy@shanghaitech.edu.cn>
2019-05-31 16:56:43 +0800

    add conv doc

--- include/ConvolutionLayer.hpp
+++ include/ConvolutionLayer.hpp
@@ -1,37 +1,51 @@
-/// \ConvolutionLayer.,hpp
-/// \brief implement  convolution layer of neural network
+/// \file ConvolutionLayer.,hpp
+/// \brief implement  convolution layer of neural network.
 #ifndef CS133FINAL_CONVOLUTIONLAYER_HPP
 #define CS133FINAL_CONVOLUTIONLAYER_HPP
 #include "Layer.hpp"
 #include <iostream>
 #include <unsupported/Eigen/CXX11/Tensor>
-/// \brief child class of Layer class
-/// convolution layer takes a 3d tensor as input and output a 3d tensor,
-/// convolution layer takes a list of  3d tensor as parameter, which are convolution kernels
+/// \brief Child class of Layer class.
+///
+/// Convolution layer takes a 3d tensor as input and output a 3d tensor,
+/// it takes a list of  3d tensors as parameter, which are convolution kernels.
 class ConvolutionLayer : public Layer
 {
     using tensor = Eigen::Tensor<double, 3>;
 
 private:
-    std::vector<tensor> kernels;                     ///<convolution kernels passed in as parameter
-    std::function<double(double)> response_function; ///<lambda functions
+    std::vector<tensor> kernels;                     ///<convolution kernels passed in as parameter.
+    std::function<double(double)> response_function; ///<lambda functions.
 
 public:
-    /// \brief constructor of convolution layer
+    /// \brief Constructor of convolution layer.
     ///\param[in] param convolution kernels
-    ///\param[in] param rf several most commonly used response functions defined in Network.hpp
+    ///\param[in] rf several most commonly used response functions defined in Network.hpp
     ConvolutionLayer(std::vector<tensor> param, std::function<double(double)> rf) : kernels(param), response_function(rf){};
-    /// \brief nothing is alloc on heap in constructor, so nothing need to manually delete in destructor
+    /// \brief Nothing is alloc on heap in constructor, so nothing need to be manually deleted in destructor.
     ~ConvolutionLayer() = default;
-    /// \brief compute convolution on 2d matrix
+    /// \brief Compute convolution on 2d matrix.
     ///
-    /// this function is invoked in 3d convolution by divilding 3d tensor to vector of 2d matrix
+    /// This function is invoked in 3d convolution by divilding 3d tensor to vector of 2d matrix
     ///\param[in] input_data data matrix which convolution will be applyed
     ///\param[in] kernel a 2d convolution kernel
+    ///
+    ///\code
+    ///int space_x = input_data.cols() - kernel.cols() + 1;
+    ///\endcode
+    ///output matrix size.x after convolution
+    ///\code
+    ///int space_y = input_data.rows() - kernel.rows() + 1;
+    ///\endcode
+    ///output matrix size.x after convolution
+    ///\code
+    ///sum = response_function(sum);
+    ///\endcode
+    /// apply response function for every value on the output matrix
     Eigen::MatrixXd conv_2d(Eigen::MatrixXd input_data, Eigen::MatrixXd kernel)
     {
-        int space_x = input_data.cols() - kernel.cols() + 1; ///<output matrix size.x after convolution
-        int space_y = input_data.rows() - kernel.rows() + 1; ///<output matrix size.y after convolution
+        int space_x = input_data.cols() - kernel.cols() + 1;
+        int space_y = input_data.rows() - kernel.rows() + 1;
         Eigen::MatrixXd result(space_x, space_y);
         for (int i = 0; i < space_x; i++)
         {
@@ -46,26 +60,47 @@ public:
                         sum += convolution_window(row, col) * kernel(row, col);
                     }
                 }
-                sum = response_function(sum); ///< apply response function for every value on the output matrix
+                sum = response_function(sum);
                 result(i, j) = sum;
             }
         }
         return result;
     }
-    ///\brief concrete implementation of abstruct definition in base class: Layer
+    ///\brief Concrete implementation of abstruct definition in base class: Layer
     ///\param[in] input 3d tensor from origin data or last layer of the neural network
     tensor calculate(tensor input)
     {
         int conv_window_x = input.dimension(1) - kernels[0].dimension(1) + 1;
         int conv_window_y = input.dimension(2) - kernels[0].dimension(2) + 1;
         tensor result(kernels.size(), conv_window_x, conv_window_y);
-        ///\brief a lambda function that turn a 3d tensor into a vector of 2d matrices
+        ///\code
+        /// auto tensor2matrix = [](tensor t) -> std::vector<Eigen::MatrixXd>
+        ///\endcode
+        /// A lambda function is used to turn a 3d tensor into a vector of 2d matrices.
+        /// Both input data and convolution kernels are needed to be turned,
+        /// using a lambda function can reuse the code  for the entire convolution layer.
         ///
-        /// both input data and convolution kernels are needed to be turned,
-        /// using a lambda function can reuse the code  for the entire convolutiion layer.
-        /// a 3d tensor has 3 dimentions which we let their size to be d0,d1,d2
-        /// in our implimentation we take d0 as the depth of tensor
-        /// so there will be d0 matrices and their width and height are all d1 and d2
+        ///\note
+        /// A 3d tensor has 3 dimentions which we let their size to be d0, d1, d2,
+        /// In our implimentation we take d0 as the depth of tensor,
+        /// so there will be d0 matrices and their width and height are all d1 and d2.
+        ///\code
+        ///    Eigen::MatrixXd frame(conv_window_x, conv_window_y);
+        ///    frame.setConstant(0.0);
+        ///    for (int index = 0; index < kernel_matrice.size(); index++)
+        ///    {
+        ///        frame += conv_2d(input_matrice[index], kernel_matrice[index]);
+        ///    }
+        ///    for (int x = 0; x < conv_window_x; x++)
+        ///    {
+        ///        for (int y = 0; y < conv_window_y; y++)
+        ///        {
+        ///            result(i, x, y) = frame(x, y);
+        ///        }
+        ///    }
+        ///\endcode
+        /// For a multi-channel input data and multi-channel convolution kernel, add up all result value from each channel.
+        /// Add the result matrix of one kernel convolution as one page of the final output tensor
         auto tensor2matrix = [](tensor t) -> std::vector<Eigen::MatrixXd> {
             std::vector<Eigen::MatrixXd> result;
             for (int depth = 0; depth < t.dimension(0); depth++)
@@ -91,13 +126,13 @@ public:
             frame.setConstant(0.0);
             for (int index = 0; index < kernel_matrice.size(); index++)
             {
-                frame += conv_2d(input_matrice[index], kernel_matrice[index]); ///< for a multi-channel input data and multi-channel convolution kernel, add up all result value from each channel
+                frame += conv_2d(input_matrice[index], kernel_matrice[index]);
             }
             for (int x = 0; x < conv_window_x; x++)
             {
                 for (int y = 0; y < conv_window_y; y++)
                 {
-                    result(i, x, y) = frame(x, y); ///< add the result matrix of one kernel convolution as one page of the final output tensor
+                    result(i, x, y) = frame(x, y);
                 }
             }
         }
